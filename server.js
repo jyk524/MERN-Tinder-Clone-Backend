8b67018fc160c8e7b796450c36f7a2a8eb32fe6d
jyk524 <67247717+jyk524@users.noreply.github.com>
2020-11-12 04:38:34 -0500

    Middleware added, Post and get at /tinder/cards and / working

--- server.js
+++ server.js
@@ -18,8 +18,9 @@ mongoose.connect(connection_url, {
 })
 
 // API Endpoints
-app.get('/', (req,res) => res.status(200).send('Hello World'));
-app.post('/tinder/card', (req, res) => {
+app.get("/", (req,res) => res.status(200).send('Hello James'));
+
+app.post("/tinder/cards", (req, res) => {
     const dbCard = req.body;
 
     Cards.create(dbCard, (err, data) => {
@@ -28,18 +29,19 @@ app.post('/tinder/card', (req, res) => {
         } else {
             res.status(201).send(data)
         }
-    })
-})
+    });
+});
 
-app.get('tinder/cards', (req, res) => {
-    Cards.find(dbCard, (err, data) => {
+    // for downloading data, retrieves all the cards we want
+app.get("/tinder/cards", (req, res) => {
+    Cards.find((err, data) => {
         if (err) {
             res.status(500).send(err)
         } else {
             res.status(200).send(data)
-        }
-    })
-})
+        };
+    });
+});
 
 // Listener
-app.listen(port, () => console.log(`listening on local host ${port}`))
+app.listen(port, () => console.log(`listening on local host ${port}`));
